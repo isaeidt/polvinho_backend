40ab451cd2728b0e0f65059d1079c2484eff84e2
isaeidt <isadorasoareseidt12@gmail.com>
2025-08-20 16:38:40 -0300

    ajuste cors

--- src/validator/app.js
+++ src/validator/app.js
@@ -1,4 +1,3 @@
-import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
@@ -9,13 +8,8 @@ import userRoute from '../routes/userRoute.js';
 import conn from '../validator/conn.js';
 
 const app = express();
-app.use(
-	cors({
-		origin: 'http://127.0.0.1:5502',
-		credentials: true,
-	}),
-);
-app.use(cookieParser());
+app.use(cors());
+
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
